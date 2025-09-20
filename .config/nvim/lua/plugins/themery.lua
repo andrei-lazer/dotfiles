06573bb94513200f0a74fafeffbd5f02a3313660
andrei-lazer <andrei.lazer319@gmail.com>
2025-09-20 10:25:08 +0100

    Better theme switching and tmux shell is now zsh

--- .config/nvim/lua/plugins/themery.lua
+++ .config/nvim/lua/plugins/themery.lua
@@ -1,15 +1,20 @@
 return
 {
-	{ 'sainnhe/everforest', },
-	{ 'ellisonleao/gruvbox' },
+	{ 'sainnhe/everforest' },
 	{
+		'ellisonleao/gruvbox.nvim',
+		priority = 1000,
+		config = true
+	},
+ 	{
 		"zaldih/themery.nvim",
 		lazy = false,
 		config = function()
 			vim.cmd("colorscheme gruvbox")
 			require("themery").setup({
-				themes = {"everforest", "gruvbox"},
+				themes = vim.fn.getcompletion("", "color"),
 				livePreview = true, -- Apply theme while picking. Default to true.
+				vim.keymap.set("n", "<leader>th", "<cmd>Themery<cr>")
 				-- add the config here
 			})
 		end
